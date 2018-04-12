090a885d8c6609d76adc7900a7cd1f1ccbffef38
Brian Sadler <sadler.brian@gmail.com>
2018-04-12 11:33:11 +0000

    Implement Coming Soon page

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ application up and running.
 
 Things you may want to cover:
 
-* Ruby version
+* Ruby 2.4.0, Rails 5.0.2
 
-* System dependencies
+* Uses gem 'bootstrap', '~> 4.0.0' for layout
 
 * Configuration
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'coming_soon/index'
+
   resources :homes
   get 'home/index'
 
-  root 'home#index'
+  #root 'home#index'
+  root 'coming_soon#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

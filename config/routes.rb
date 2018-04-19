0325618565eaf79615069b59bc5fe1ac41597a9b
sadleb <sadler.brian@gmail.com>
2018-04-19 11:39:29 +0000

    Upgrade to Rails 5.2.0 and Ruby 2.5.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
+
   get 'coming_soon/index'
-
-  resources :homes
-  get 'home/index'
-
-  #root 'home#index'
   root 'coming_soon#index'
+
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

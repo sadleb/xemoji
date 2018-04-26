5b64f41f71ada489238fee19d7f1b04616107682
Brian Sadler <brian.sadler@beyondz.org>
2018-04-25 20:27:32 -0400

    Create signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   get 'coming_soon/index'
   #root 'coming_soon#index'
   root 'pages#index'
 
   resources :users
 
+  # Registration, login routes.
+  devise_for :users, :controllers => {:registrations => "registrations"}
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
